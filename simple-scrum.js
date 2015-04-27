b3353be58edf3f0fca5f221925f7f3dc2b5ea028
seraya <takahser@students.zhaw.ch>
2015-04-27 13:03:45 +0200

    added meteor, static helpers for tasks, basic templates

---
+++ simple-scrum.js
@@ -0,0 +1,28 @@
+// Meteor client code
+if (Meteor.isClient) {
+
+  // You can pass data into templates from your JavaScript code by defining helpers.
+  Template.body.helpers({
+    tasks: [
+      {
+        name : 'Learn AngularJs',
+        info : 'Blubb',
+        estimation: 1,
+        status: 'ToDo',
+        personResponsible: 'Chuck Norris'
+      },{
+        name : 'Learn NodeJs',
+        info : 'Blah',
+        estimation: 5,
+        status: 'InProgress',
+        personResponsible: 'Jackie Chan'
+      },{
+        name : 'Learn Meteor',
+        info : 'huh',
+        estimation: 3,
+        status: 'Done',
+        personResponsible: 'Bruce Lee'
+      }
+    ]
+  });
+}
